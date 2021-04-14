dde34c9e9a05b452cbe427dc9129c23011e3cf3c
Drew Keat <thedrewkeat@gmail.com>
2021-04-14 10:19:58 -0600

    commit tree 0673b80a5995f7549828e33e28d70d36dca67e36

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,12 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  my_hash = {
+    "Unit 1" => "Ensemble",
+    "Unit 2" => "Improv",
+    "Unit 3" => "Acting",
+    "Unit 4" => "Design",
+    "Unit 5" => "Production",
+  }
   
 end
 
@@ -9,7 +15,11 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
+  the_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3,
+  }
 
 end
 
